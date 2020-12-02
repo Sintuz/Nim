3786f1efb00006cbb44c0159e5a1b0510d602348
Sintuz <enrico.sintoni.ra@gmail.com>
2020-12-02 18:01:09 +0100

    Update link name and added target to blank page

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <?php
 echo "<footer>";
 echo "<p>Author: Sintoni Enrico</p>";
-echo "<p>Repository: <a href='https://www.github.com/Sintuz/Nim'>Sintuz/Nim</a></p>";
+echo "<p>Repository: <a href='https://www.github.com/Sintuz/Nim' target='_blank'>@Sintuz/Nim</a></p>";
 echo "</footer>";
